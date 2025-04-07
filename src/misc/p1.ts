fe0a882fcee97dc9183403cf0b21fdd621cc5b56
anubhav bisht <anubhavbisht98@gmail.com>
2025-04-07 16:20:59 +0530

    learning assertion type and generic map

---
+++ src/misc/p1.ts
@@ -0,0 +1,16 @@
+export const submitData = <T extends { id: number, title: string }>(data: T) => {
+    return {
+        ...data,
+        timestamp: new Date(),
+    };
+};
+
+const data = [
+    {
+        id: 123,
+        title: "This is a fake title!",
+    },
+];
+
+const submittedUsers = data.map(submitData);
+console.log(submittedUsers)
